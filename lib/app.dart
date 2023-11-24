63621112e0d8cc3677cffeb1993823c06296193c
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-11-24 20:08:35 +0530

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,129 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:group_maker/group_maker.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> names = [
-    'John',
-    'Jane',
-    'Bob',
-    'Alice',
-    'Charlie',
-    'Eva',
-    'David',
-    'Emma',
-    'Frank',
-    'Grace',
-  ];
-
-  List<List<String>> groups = [];
-
-  final TextEditingController nameController = TextEditingController();
-
-  void buildTeams() {
-    names.shuffle();
-    int groupSize = 5;
-    int numberOfTeams = names.length ~/ groupSize;
-    int remainingPlayers = names.length % groupSize;
-
-    groups.clear();
-
-    for (int i = 0; i < numberOfTeams; i++) {
-      int endIndex = i * groupSize + groupSize;
-      groups.add(names.sublist(i * groupSize, endIndex));
-    }
-
-    if (remainingPlayers > 0 && remainingPlayers <= 3) {
-      for (int i = 0; i < remainingPlayers; i++) {
-        groups[i].add(names[numberOfTeams * groupSize + i]);
-      }
-    } else if (remainingPlayers > 3) {
-      int remainingCount = remainingPlayers;
-      for (int i = 0; i < numberOfTeams; i++) {
-        if (remainingCount > 0) {
-          groups[i].add(names[numberOfTeams * groupSize + i]);
-          remainingCount--;
-        }
-      }
-    }
-  }
+class GroupMaker extends StatelessWidget {
+  const GroupMaker({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(
-              color: Colors.blue[700],
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Group Count : ${groups.length} | Student Count ${names.length}',
-                      style: const TextStyle(
-                          fontSize: 20,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                labelText: 'Enter Name',
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Group Maker'),
+        elevation: 0,
+        actions: [
+          IconButton(
               onPressed: () {
-                String newName = nameController.text.trim();
-                if (newName.isNotEmpty && !names.contains(newName)) {
-                  setState(() {
-                    names.add(newName);
-                    nameController.clear();
-                    buildTeams();
-                  });
-                }
+                Navigator.pushNamed(context, '/home_page');
               },
-              child: const Text('Add Name'),
+              icon: const Icon(Icons.logout))
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const DrawerHeader(
+                child: Icon(
+              Icons.favorite,
+              size: 48,
+            )),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('H o m e'),
+              onTap: () {
+                Navigator.pushNamed(context, '/home_page');
+              },
             ),
-            const SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: groups.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      title: Text(
-                        'Group | 0${index + 1}',
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:
-                            groups[index].map((name) => Text(name)).toList(),
-                      ),
-                    ),
-                  );
-                },
-              ),
+           const ListTile(
+              leading: Icon(Icons.info),
+              title: Text('A b o u t'),
+            ),
+           const ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('S e t t i n g s'),
             ),
           ],
         ),
       ),
+      body: const MyHomePage(),
     );
   }
 }
--- lib/group_maker.dart
+++ lib/group_maker.dart
@@ -1,50 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:group_maker/app.dart';
 
-class GroupMaker extends StatelessWidget {
-  const GroupMaker({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> names = [
+    'John',
+    'Jane',
+    'Bob',
+    'Alice',
+    'Charlie',
+    'Eva',
+    'David',
+    'Emma',
+    'Frank',
+    'Grace',
+  ];
+
+  List<List<String>> groups = [];
+
+  final TextEditingController nameController = TextEditingController();
+
+  void buildTeams() {
+    names.shuffle();
+    int groupSize = 5;
+    int numberOfTeams = names.length ~/ groupSize;
+    int remainingPlayers = names.length % groupSize;
+
+    groups.clear();
+
+    for (int i = 0; i < numberOfTeams; i++) {
+      int endIndex = i * groupSize + groupSize;
+      groups.add(names.sublist(i * groupSize, endIndex));
+    }
+
+    if (remainingPlayers > 0 && remainingPlayers <= 3) {
+      for (int i = 0; i < remainingPlayers; i++) {
+        groups[i].add(names[numberOfTeams * groupSize + i]);
+      }
+    } else if (remainingPlayers > 3) {
+      int remainingCount = remainingPlayers;
+      for (int i = 0; i < numberOfTeams; i++) {
+        if (remainingCount > 0) {
+          groups[i].add(names[numberOfTeams * groupSize + i]);
+          remainingCount--;
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Group Maker'),
-        elevation: 0,
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home_page');
-              },
-              icon: const Icon(Icons.logout))
-        ],
-      ),
-      drawer: Drawer(
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const DrawerHeader(
-                child: Icon(
-              Icons.favorite,
-              size: 48,
-            )),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('H o m e'),
-              onTap: () {
-                Navigator.pushNamed(context, '/home_page');
+            Card(
+              color: Colors.blue[700],
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Group Count : ${groups.length} | Student Count ${names.length}',
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: 'Enter Name',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String newName = nameController.text.trim();
+                if (newName.isNotEmpty && !names.contains(newName)) {
+                  setState(() {
+                    names.add(newName);
+                    nameController.clear();
+                    buildTeams();
+                  });
+                }
               },
+              child: const Text('Add Name'),
             ),
-           const ListTile(
-              leading: Icon(Icons.info),
-              title: Text('A b o u t'),
-            ),
-           const ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('S e t t i n g s'),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: groups.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(
+                        'Group | 0${index + 1}',
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:
+                            groups[index].map((name) => Text(name)).toList(),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      body: const MyHomePage(),
     );
   }
 }
